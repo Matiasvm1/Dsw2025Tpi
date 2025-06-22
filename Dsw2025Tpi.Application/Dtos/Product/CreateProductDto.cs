af15fc98341f91e48f60c9eef3f98e264cacad0c
Matias Villafañe <matutevm1@gmail.com>
2025-06-22 17:39:06 -0300

    feat(application): agrega DTOs, validadores y servicio para el manejo de productos

---
+++ Dsw2025Tpi.Application/Dtos/Product/CreateProductDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Application.Dtos.Product
+{
+    public class CreateProductDto
+    {
+        public string Sku { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public string? InternalCode { get; set; }
+        public decimal CurrentUnitPrice { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
---
+++ Dsw2025Tpi.Application/Interfaces/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dsw2025Tpi.Application.Dtos.Product;
+
+namespace Dsw2025Tpi.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductViewDto>> GetAllAsync();
+        Task<ProductViewDto?> GetByIdAsync(Guid id);
+        Task<ProductViewDto> CreateAsync(CreateProductDto dto);
+        Task<ProductViewDto?> UpdateAsync(UpdateProductDto dto);
+        Task DisableAsync(Guid id);
+    }
+}
